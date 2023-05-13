7eeccf7ad923930ef66a96075f91e9dbc6040baf
hiisAnny <wefu5045@uni.sydney.edu.au>
2023-05-13 14:12:59 +1000

    3dGround

---
+++ 3D_Module/3D.js
@@ -0,0 +1,33 @@
+let scene, camera, renderer;
+        const groundWidth = 1000;
+        const groundLength = 1000;
+        const groundHeight = 400;
+
+        init();
+        // animate();
+        render();
+
+        function init() {
+            // 创建场景
+            scene = new THREE.Scene();
+            // scene.background = new THREE.Color(0xffffff);
+
+            // 创建相机
+            camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+            camera.position.set(300, 990, 800);
+            camera.lookAt(0, 0, 0);
+
+            // 创建地皮
+            const groundGeometry = new THREE.BoxGeometry(groundWidth, groundHeight, groundLength);
+            const groundMaterial = new THREE.MeshBasicMaterial({ color: 0xafcef3 });
+            const ground = new THREE.Mesh(groundGeometry, groundMaterial);
+            scene.add(ground);
+            // 创建渲染器
+            renderer = new THREE.WebGLRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight);
+            document.body.appendChild(renderer.domElement);
+            renderer.setClearColor(0xffffff);
+        }
+        function render() {
+            renderer.render(scene, camera); // 将场景渲染到界面
+        }
