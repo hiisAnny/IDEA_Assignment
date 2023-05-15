d88f6948cfb6da946e99429755b30848e22b03e1
hiisAnny <wefu5045@uni.sydney.edu.au>
2023-05-15 23:52:29 +1000

    v4. 点击开始，产生火焰

--- Forest/forest.js
+++ Forest/forest.js
@@ -9,21 +9,16 @@ let pointer, raycaster, isShiftDown = false;
 let rollOverMesh, rollOverMaterial;
 let cubeGeo, cubeMaterial;
 
-let fire;
-const fireAreas = [];
-
 let gridHelper, controls;
 
 //需要进行射线相交检测的对象
 const objects = [];
 let density = [];
 
-
-
 init();
 render();
 
-export { scene, objects, density };
+export { scene, objects, density, cubeGeo, cubeMaterial };
 
 function init() {
     const forest = document.getElementById("forest");
@@ -71,22 +66,6 @@ function init() {
     backgroundGeometry.rotateX(- Math.PI / 2);
     backgroundPlane.position.set(0, -1, 0); // 将背景平面设置在网格的后面
     scene.add(backgroundPlane);
-
-    fire = new THREE.PointLight(0xff0000, 1, 100);
-    fire.add(new THREE.Mesh(new THREE.SphereGeometry(15, 12, 2), new THREE.MeshBasicMaterial({ color: 0xff0000 })));
-    fire.position.set(20, 50, 0);
-    scene.add(fire);
-
-
-    const range = fire.distance;
-    console.log("火光的范围哦：", range); // 输出点光源的范围值
-    const fireAreaGeometry = new THREE.PlaneGeometry(range, range);
-    const fireAreaMaterial = new THREE.MeshStandardMaterial({ color: 0xffa8a8, visible: true });
-    const fireArea = new THREE.Mesh(fireAreaGeometry, fireAreaMaterial);
-    fireArea.rotateX(- Math.PI / 2);
-    scene.add(fireArea);
-    fireAreas.push(fireArea);
-    console.log("被点着的有：", fireAreas);
 
     // 环境光
     const ambientLight = new THREE.AmbientLight(0x606060);
@@ -221,94 +200,12 @@ function onDocumentKeyUp(event) {
 
 }
 
-// 更新函数，用于每一帧更新小球的旋转
-function updateRotation() {
-    // 每一帧更新小球的旋转角度
-    fire.rotation.x += 0.05; // 绕X轴旋转
-    fire.rotation.y += 0.01; // 绕Y轴旋转
-    setInterval(updateRotation, 100); // 1000毫秒，即1秒
-}
-
-// 判断立方体是否在火焰范围内
-function isTreeInFireArea(fireArea, tree) {
-    // 获取平面的边界框或包围盒
-    const boundingBox = new THREE.Box3().setFromObject(fireArea);
-
-    // 获取最小点和最大点的坐标
-    const minPoint = boundingBox.min;
-    const maxPoint = boundingBox.max;
-
-    // // 输出平面的范围
-    // console.log('平面的范围：');
-    // console.log('最小点坐标：', minPoint);
-    // console.log('最大点坐标：', maxPoint);
-
-    const boundingBox2 = new THREE.Box3().setFromObject(tree);
-    // 获取最小点和最大点的坐标
-    const minPoint2 = boundingBox2.min;
-    const maxPoint2 = boundingBox2.max;
-
-    // // 输出平面的范围
-    // console.log('树的范围：');
-    // console.log('最小点坐标：', minPoint2);
-    //     console.log('最大点坐标：', maxPoint2);
-
-    // 判断立方体的边界框是否与平面的范围相交
-    const isCubeAbovePlane =
-        minPoint2.x <= maxPoint.x &&
-        maxPoint2.x >= minPoint.x &&
-        minPoint2.y <= maxPoint.y &&
-        maxPoint2.y >= minPoint.y &&
-        minPoint2.z <= maxPoint.z &&
-        maxPoint2.z >= minPoint.z;
-    console.log('立方体是否在平面的上方：', isCubeAbovePlane);
-    if (isCubeAbovePlane) {
-        const index = objects.indexOf(tree);
-        if (index != -1) {
-            scene.remove(tree);
-            objects.splice(index, 1);
-        }
-    }
-}
-
 export function render() {
-    if (density.length > 0) {
-        // 遍历 objects 数组，对每个立方体进行判断
-        fireAreas.forEach(fireArea => {
-            density.forEach(tree => {
-                isTreeInFireArea(fireArea, tree)
-            });
-        });
-    }
-
     // 遍历 objects 数组，对每个立方体进行判断
-
-    updateRotation();
     controls.update();
     renderer.render(scene, camera);
-
 }
 
-export function addRandomCube() {
-    //   console.log('Position:', plane.position);
-    //   console.log('Scale:', plane.scale);
-    const positionX = randomPosition(-425, 425);
-    const positionY = 20;// 平面所在的高度
-    const positionZ = randomPosition(-425, 425);
 
-    const newGeo = new THREE.BoxGeometry(50, 50, 50);
-    const newCube = new THREE.Mesh(newGeo, cubeMaterial);
-    newCube.position.set(positionX, positionY, positionZ);
 
-    // 将立方体添加到场景中
-    scene.add(newCube);
-    objects.push(newCube);
-    density.push(newCube);
 
-    // console.log("用了吗")
-    render();
-}
-
-function randomPosition(min, max) {
-    return Math.random() * (max - min) + min
-}
--- Forest/interaction.js
+++ Forest/interaction.js
@@ -1,5 +1,10 @@
-import { scene, objects, density } from './forest.js';
-import { addRandomCube, render } from './forest.js';
+import * as THREE from 'three';
+import { scene, objects, density, cubeGeo, cubeMaterial } from './forest.js';
+import { render } from './forest.js';
+
+let fire;
+const fireAreas = [];
+
 const startButton = document.getElementById('startButton');
 startButton.addEventListener('click', startSimulation);
 
@@ -10,13 +15,13 @@ const decreaseButton = document.getElementById('decreaseDensity');
 decreaseButton.addEventListener('click', decreaseDensity);
   
 function startSimulation() {
-    
-    console.log(density);
+    createFire();
 }  
 
 function increaseDensity() {
     addRandomCube();
 }  
+
 function decreaseDensity() {
     // plane平面
     // console.log(objects[0]); 
@@ -29,3 +34,105 @@ function decreaseDensity() {
         render();
     }
 } 
+
+function randomPosition(min, max) {
+    return Math.random() * (max - min) + min
+}
+
+function addRandomCube() {
+    //   console.log('Position:', plane.position);
+    //   console.log('Scale:', plane.scale);
+    const positionX = randomPosition(-425, 425);
+    const positionY = 20;// 平面所在的高度
+    const positionZ = randomPosition(-425, 425);
+
+    const newCube = new THREE.Mesh(cubeGeo, cubeMaterial);
+    newCube.position.set(positionX, positionY, positionZ);
+
+    // 将立方体添加到场景中
+    scene.add(newCube);
+    objects.push(newCube);
+    density.push(newCube);
+
+    render();
+}
+
+function createFire() {
+    fire = new THREE.PointLight(0xff0000, 1, 100);
+    fire.add(new THREE.Mesh(new THREE.SphereGeometry(15, 12, 2), new THREE.MeshBasicMaterial({ color: 0xff0000 })));
+    fire.position.set(20, 50, 0);
+    scene.add(fire);
+    
+    const range = fire.distance;
+    const fireAreaGeometry = new THREE.PlaneGeometry(range, range);
+    const fireAreaMaterial = new THREE.MeshStandardMaterial({ color: 0xffa8a8, visible: true });
+    const fireArea = new THREE.Mesh(fireAreaGeometry, fireAreaMaterial);
+    fireArea.rotateX(- Math.PI / 2);
+
+    scene.add(fireArea);
+    fireAreas.push(fireArea);
+
+    console.log("火光的范围：", range); // 输出点光源的范围值
+    console.log("被点着的有：", fireAreas);
+
+    if (density.length > 0 && fireAreas.length > 0) {
+        // 遍历每个平面，对每个立方体进行判断
+        fireAreas.forEach(fireArea => {
+            density.forEach(tree => {
+                isTreeInFireArea(fireArea, tree)
+            });
+        });
+    }
+}
+
+// 更新函数，用于每一帧更新小球的旋转
+function updateRotation() {
+    // 每一帧更新小球的旋转角度
+    fire.rotation.x += 0.05; // 绕X轴旋转
+    fire.rotation.y += 0.01; // 绕Y轴旋转
+    setInterval(updateRotation, 100); // 1000毫秒，即1秒
+}
+
+// 判断立方体是否在火焰范围内
+function isTreeInFireArea(fireArea, tree) {
+
+    // 获取平面的边界框或包围盒
+    const boundingBox = new THREE.Box3().setFromObject(fireArea);
+
+    // 获取最小点和最大点的坐标
+    const minPoint = boundingBox.min;
+    const maxPoint = boundingBox.max;
+
+    // // 输出平面的范围
+    // console.log('平面的范围：');
+    // console.log('最小点坐标：', minPoint);
+    // console.log('最大点坐标：', maxPoint);
+
+    const boundingBox2 = new THREE.Box3().setFromObject(tree);
+    // 获取最小点和最大点的坐标
+    const minPoint2 = boundingBox2.min;
+    const maxPoint2 = boundingBox2.max;
+
+    // // 输出平面的范围
+    // console.log('树的范围：');
+    // console.log('最小点坐标：', minPoint2);
+    //     console.log('最大点坐标：', maxPoint2);
+
+    // 判断立方体的边界框是否与平面的范围相交
+    const isCubeAbovePlane =
+        minPoint2.x <= maxPoint.x &&
+        maxPoint2.x >= minPoint.x &&
+        minPoint2.y <= maxPoint.y &&
+        maxPoint2.y >= minPoint.y &&
+        minPoint2.z <= maxPoint.z &&
+        maxPoint2.z >= minPoint.z;
+    console.log('立方体是否在平面的上方：', isCubeAbovePlane);
+    if (isCubeAbovePlane) {
+        const index = objects.indexOf(tree);
+        if (index != -1) {
+            objects.splice(index, 1);
+            density.splice(density.indexOf(tree), 1);
+            scene.remove(tree);
+        }
+    }
+}
